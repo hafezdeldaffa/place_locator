5e9bf0f7135bca12c332446b1ae86f4dde98002f
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2021-03-06 21:24:45 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Scaffold(
+      appBar: AppBar(
+        title: Text('Place Locator'),
+        actions: [
+          IconButton(icon: Icon(Icons.add), onPressed: () {})
+        ],
+      ),
+    );
+  }
+}
